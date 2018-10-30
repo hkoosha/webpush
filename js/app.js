760f32f1767c7e8f020a05eb93c4672439587b7b
Stathis Papadopoulos <efpapado@eksoria.gr>
2018-10-30 13:57:57 +0100

    Refactor js.

--- js/app.js
+++ js/app.js
@@ -63,7 +63,7 @@
       return outputArray;
     },
 
-    push_sendSubscriptionToServer: function (subscription, method) {
+    push_sendSubscriptionToServer: function (subscription, method, data) {
       const key = subscription.getKey('p256dh');
       const token = subscription.getKey('auth');
       const contentEncoding = (PushManager.supportedContentEncodings || ['aesgcm'])[0];
@@ -76,6 +76,7 @@
           publicKey: key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : null,
           authToken: token ? btoa(String.fromCharCode.apply(null, new Uint8Array(token))) : null,
           contentEncoding,
+          data: data
         }),
       }).then(() => subscription);
     },
@@ -122,6 +123,95 @@
       }
 
     },
+
+
+    push_updateSubscription: function (data) {
+      const that = this;
+      navigator.serviceWorker.ready.then(serviceWorkerRegistration => serviceWorkerRegistration.pushManager.getSubscription())
+          .then(subscription => {
+            that.updateWebpushState('disabled');
+
+            if (!subscription) {
+              // We aren't subscribed to push, so set UI to allow the user to
+              // enable push
+              return;
+            }
+
+            // Keep your server in sync with the latest endpoint
+            return that.push_sendSubscriptionToServer(subscription, 'PUT', data);
+          })
+          .then(subscription => subscription && that.updateWebpushState('enabled')) // Set your UI to show they have subscribed for push messages
+          .catch(e => {
+            console.error('Error when updating the subscription', e);
+          });
+    },
+
+    push_subscribe: function (data) {
+      const that = this;
+      this.updateWebpushState('computing');
+
+      navigator.serviceWorker.ready
+          .then(serviceWorkerRegistration => serviceWorkerRegistration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: that.urlBase64ToUint8Array(that.applicationServerKey),
+          }))
+          .then(subscription => {
+            // Subscription was successful
+            // create subscription on your server
+            return that.push_sendSubscriptionToServer(subscription, 'POST', data);
+          })
+          .then(subscription => subscription && that.updateWebpushState('enabled')) // update your UI
+          .catch(e => {
+            if (Notification.permission === 'denied') {
+              // The user denied the notification permission which
+              // means we failed to subscribe and the user will need
+              // to manually change the notification permission to
+              // subscribe to push messages
+              console.warn('Notifications are denied by the user.');
+              that.updateWebpushState('userdenied');
+            }
+            else {
+              // A problem occurred with the subscription; common reasons
+              // include network errors or the user skipped the permission
+              console.error('Impossible to subscribe to push notifications', e);
+              that.updateWebpushState('disabled');
+            }
+          });
+    },
+
+    push_unsubscribe: function (data) {
+      const that = this;
+      this.updateWebpushState('computing');
+
+      // To unsubscribe from push messaging, you need to get the subscription
+      // object
+      navigator.serviceWorker.ready
+          .then(serviceWorkerRegistration => serviceWorkerRegistration.pushManager.getSubscription())
+          .then(subscription => {
+            // Check that we have a subscription to unsubscribe
+            if (!subscription) {
+              // No subscription object, so set the state
+              // to allow the user to subscribe to push
+              that.updateWebpushState('disabled');
+              return;
+            }
+
+            // We have a subscription, unsubscribe
+            // Remove push subscription from server
+            return that.push_sendSubscriptionToServer(subscription, 'DELETE', data);
+          })
+          .then(subscription => subscription.unsubscribe())
+          .then(() => that.updateWebpushState('disabled'))
+          .catch(e => {
+            // We failed to unsubscribe, this can lead to
+            // an unusual state, so  it may be best to remove
+            // the users data from your data store and
+            // inform the user that you have done so
+            console.error('Error when unsubscribing the user', e);
+            that.updateWebpushState('disabled', $subButton);
+          });
+    },
+
 
   };
 })(jQuery, Drupal);
--- js/simple-button.js
+++ js/simple-button.js
@@ -23,7 +23,7 @@
       navigator.serviceWorker.register("webpush/serviceworker/js", {scope: '/'})
           .then(() => {
             console.log('[SW] Service worker has been registered');
-            Drupal.behaviors.webPushSimpleButton.fn.push_updateSubscription();
+            Drupal.behaviors.webPushApp.push_updateSubscription();
           }, e => {
             console.error('[SW] Service worker registration failed', e);
             Drupal.behaviors.webPushApp.updateWebpushState('incompatible');
@@ -46,10 +46,10 @@
           $(this).click(function () {
             const $button = $(this);
             if (Drupal.behaviors.webPushApp.isPushEnabled) {
-              Drupal.behaviors.webPushSimpleButton.fn.push_unsubscribe({button: $button});
+              Drupal.behaviors.webPushApp.push_unsubscribe({button: $button});
             }
             else {
-              Drupal.behaviors.webPushSimpleButton.fn.push_subscribe({button: $button});
+              Drupal.behaviors.webPushApp.push_subscribe({button: $button});
             }
           });
         });
@@ -58,89 +58,8 @@
       },
 
 
-      push_updateSubscription: function () {
-        navigator.serviceWorker.ready.then(serviceWorkerRegistration => serviceWorkerRegistration.pushManager.getSubscription())
-            .then(subscription => {
-              Drupal.behaviors.webPushApp.updateWebpushState('disabled');
 
-              if (!subscription) {
-                // We aren't subscribed to push, so set UI to allow the user to
-                // enable push
-                return;
-              }
 
-              // Keep your server in sync with the latest endpoint
-              return Drupal.behaviors.webPushApp.push_sendSubscriptionToServer(subscription, 'PUT');
-            })
-            .then(subscription => subscription && Drupal.behaviors.webPushApp.updateWebpushState('enabled')) // Set your UI to show they have subscribed for push messages
-            .catch(e => {
-              console.error('Error when updating the subscription', e);
-            });
-      },
-
-      push_subscribe: function (data) {
-        Drupal.behaviors.webPushApp.updateWebpushState('computing');
-
-        navigator.serviceWorker.ready
-            .then(serviceWorkerRegistration => serviceWorkerRegistration.pushManager.subscribe({
-              userVisibleOnly: true,
-              applicationServerKey: Drupal.behaviors.webPushApp.urlBase64ToUint8Array(Drupal.behaviors.webPushApp.applicationServerKey),
-            }))
-            .then(subscription => {
-              // Subscription was successful
-              // create subscription on your server
-              return Drupal.behaviors.webPushApp.push_sendSubscriptionToServer(subscription, 'POST');
-            })
-            .then(subscription => subscription && Drupal.behaviors.webPushApp.updateWebpushState('enabled')) // update your UI
-            .catch(e => {
-              if (Notification.permission === 'denied') {
-                // The user denied the notification permission which
-                // means we failed to subscribe and the user will need
-                // to manually change the notification permission to
-                // subscribe to push messages
-                console.warn('Notifications are denied by the user.');
-                Drupal.behaviors.webPushApp.updateWebpushState('userdenied');
-              }
-              else {
-                // A problem occurred with the subscription; common reasons
-                // include network errors or the user skipped the permission
-                console.error('Impossible to subscribe to push notifications', e);
-                Drupal.behaviors.webPushApp.updateWebpushState('disabled');
-              }
-            });
-      },
-
-      push_unsubscribe: function (data) {
-        Drupal.behaviors.webPushApp.updateWebpushState('computing');
-
-        // To unsubscribe from push messaging, you need to get the subscription
-        // object
-        navigator.serviceWorker.ready
-            .then(serviceWorkerRegistration => serviceWorkerRegistration.pushManager.getSubscription())
-            .then(subscription => {
-              // Check that we have a subscription to unsubscribe
-              if (!subscription) {
-                // No subscription object, so set the state
-                // to allow the user to subscribe to push
-                Drupal.behaviors.webPushApp.updateWebpushState('disabled');
-                return;
-              }
-
-              // We have a subscription, unsubscribe
-              // Remove push subscription from server
-              return Drupal.behaviors.webPushApp.push_sendSubscriptionToServer(subscription, 'DELETE');
-            })
-            .then(subscription => subscription.unsubscribe())
-            .then(() => Drupal.behaviors.webPushApp.updateWebpushState('disabled'))
-            .catch(e => {
-              // We failed to unsubscribe, this can lead to
-              // an unusual state, so  it may be best to remove
-              // the users data from your data store and
-              // inform the user that you have done so
-              console.error('Error when unsubscribing the user', e);
-              Drupal.behaviors.webPushApp.updateWebpushState('disabled', $subButton);
-            });
-      },
 
     },
 
